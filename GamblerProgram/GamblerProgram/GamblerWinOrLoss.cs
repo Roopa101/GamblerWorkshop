89f0ebd2b404b03b64bcb0083dc5cdb99b9977c4
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-22 11:13:55 +0530

    [Roopa] Added:Checked Win or loss

---
+++ GamblerProgram/GamblerProgram/GamblerWinOrLoss.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblerProgram
+{
+    class GamblerWinOrLoss
+    {
+        public void WinOrLoss()
+        {
+            int Start = 100;
+
+            Random random = new Random();
+
+            int num = random.Next(2);
+
+            if (num == 1)
+            {
+                Start++;
+
+                Console.WriteLine("Gambler Win the $1 bet");
+            }
+            else
+            {
+                Start--;
+                Console.WriteLine("Gambler Loss the $1 bet");
+            }
+
+        }
+    }
+}
+    
--- GamblerProgram/GamblerProgram/Program.cs
+++ GamblerProgram/GamblerProgram/Program.cs
@@ -10,14 +10,13 @@ namespace GamblerProgram
     {
         public static void Main(string[] args)
         {
-            double cash;
-            double betAmount;
-            Console.WriteLine("Welcome to Gambler Game");
-            Console.WriteLine("Please Enter Cash Amount");
-            cash = double.Parse(Console.ReadLine());
-            Console.WriteLine("Please Enter Bet Amount");
-            betAmount = double.Parse(Console.ReadLine());
+            Console.WriteLine("Welcome to Gambler Progarm ");
+
+            GamblerWinOrLoss gamblerWinOrLoss = new GamblerWinOrLoss();
+
+            gamblerWinOrLoss.WinOrLoss();
+            Console.ReadLine();
         }
     }
 }
-        
+
