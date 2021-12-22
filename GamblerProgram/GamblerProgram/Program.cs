985710ff4981f22c3af51841f02752463b161d32
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-22 09:03:38 +0530

    [Roopa] Added:UC1 done

---
+++ GamblerProgram/GamblerProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblerProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double cash;
+            double betAmount;
+            Console.WriteLine("Welcome to Gambler Game");
+            Console.WriteLine("Please Enter Cash Amount");
+            cash = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter Bet Amount");
+            betAmount = double.Parse(Console.ReadLine());
+        }
+    }
+}
+        
